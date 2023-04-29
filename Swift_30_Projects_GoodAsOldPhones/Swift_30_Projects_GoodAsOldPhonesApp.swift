221f3ee66468d56616496b52cd64058a8f460879
prettyturtle <unifconti@gmail.com>
2023-04-30 01:13:15 +0900

    Initial Commit

---
+++ Swift_30_Projects_GoodAsOldPhones/Swift_30_Projects_GoodAsOldPhonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_30_Projects_GoodAsOldPhonesApp.swift
+//  Swift_30_Projects_GoodAsOldPhones
+//
+//  Created by yc on 2023/04/30.
+//
+
+import SwiftUI
+
+@main
+struct Swift_30_Projects_GoodAsOldPhonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
